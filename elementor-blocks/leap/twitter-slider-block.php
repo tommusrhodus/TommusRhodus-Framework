a964fc86c2b67a1dd9341a71e62a9ee5d7f1ef12
Daniel Jones <distinctivedan@outlook.com>
2019-05-28 18:08:28 +0100

    Tweaking Twitter Block

--- elementor-blocks/leap/twitter-slider-block.php
+++ elementor-blocks/leap/twitter-slider-block.php
@@ -29,7 +29,22 @@ class Widget_TommusRhodus_Twitter_Slider_Block extends Widget_Base {
 		
 		$this->start_controls_section(
 			'section_my_custom', [
-				'label' => esc_html__( 'Slider Settings', 'tr-framework' ),
+				'label' => esc_html__( 'Feed Settings', 'tr-framework' ),
+			]
+		);
+
+		$this->add_control(
+			'layout', [
+				'label'   => __( 'Layout', 'tr-framework' ),
+				'type'    => Controls_Manager::SELECT,
+				'default' => 'slider',
+				'label_block' => true,
+				'options' => [
+					'slider'          		=> esc_html__( 'Slider', 'tr-framework' ),
+					'slider_widget'			=> esc_html__( 'Slider Widget', 'tr-framework' ),
+					'cards_1'				=> esc_html__( 'Cards 1', 'tr-framework' ),
+					'cards_2'				=> esc_html__( 'Cards 2', 'tr-framework' ),
+				],
 			]
 		);
 
@@ -55,15 +70,65 @@ class Widget_TommusRhodus_Twitter_Slider_Block extends Widget_Base {
 			echo '<div data-aos="fade-up" data-aos-delay="NaN">';
 		}
 
-		echo ';
-			<div data-twitter-fetcher data-username="'. $settings['twitter_username'] .'" data-twitter-flickity=\'{"wrapAround": true, "autoPlay":true}\' class="text-center">
-				<div class="carousel-cell">
-					<div class="user mb-3"></div>
-					<span class="h3 tweet d-block"></span>
-					<span class="text-small timePosted"></span>
+		if( 'slider' == $settings['layout'] ) {
+
+			echo ';
+				<div data-twitter-fetcher data-username="'. $settings['twitter_username'] .'" data-twitter-flickity=\'{"wrapAround": true, "autoPlay":true}\' class="text-center">
+					<div class="carousel-cell">
+						<div class="user mb-3"></div>
+						<span class="h3 tweet d-block"></span>
+						<span class="text-small timePosted"></span>
+					</div>
 				</div>
-			</div>
-         ';
+	         ';
+
+	     } elseif( 'slider_widget' == $settings['layout'] ) {
+
+			echo '
+				<div data-twitter-flickity=\'{"wrapAround": true, "autoPlay": false, "prevNextButtons": false, "adaptiveHeight": true}\' data-twitter-fetcher data-username="'. $settings['twitter_username'] .'">
+					<div class="carousel-cell">
+						<div class="tweet mb-2"></div>
+						<div class="d-flex align-items-center">
+							'. tommusrhodus_svg_icons_pluck( 'Twitter icon', 'icon icon-sm mr-2 opacity-30' ) .'
+							<div class="timePosted text-small"></div>
+						</div>
+					</div>
+				</div>
+	         ';
+
+	     } elseif( 'cards_1' == $settings['layout'] ) {
+
+			echo '
+				<div class="row" data-twitter-fetcher data-username="'. $settings['twitter_username'] .'" data-twitter-isotope data-sort-ascending="true">
+					<div class="col-md-4" data-isotope-item>
+						<div class="card card-body align-items-start">
+							'. tommusrhodus_svg_icons_pluck( 'Twitter icon', 'icon mb-3 bg-primary' ) .'
+							<p class="tweet w-100"></p>
+							<span class="text-small timePosted"></span>
+						</div>
+					</div>
+				</div>
+	         ';
+
+	     } elseif( 'cards_2' == $settings['layout'] ) {
+
+			echo '
+				<div class="row" data-twitter-fetcher data-username="'. $settings['twitter_username'] .'" data-twitter-isotope data-sort-ascending="true">
+					<div class="col-md-6" data-isotope-item>
+						<div class="card card-body flex-row">
+							<div class="icon-round icon-round-sm bg-primary">
+								'. tommusrhodus_svg_icons_pluck( 'Twitter icon', 'icon bg-primary' ) .'
+							</div>
+							<div class="ml-3">
+								<p class="tweet"></p>
+								<span class="text-small timePosted"></span>
+							</div>
+						</div>
+					</div>
+				</div>
+	         ';
+
+	     }
 		
 		if( !$user_selected_animation ){
 			echo '</div>';
