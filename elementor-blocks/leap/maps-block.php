6f5fc14830ffc63571b68c1ce483f7005c6cbb12
Daniel Jones <distinctivedan@outlook.com>
2019-05-28 14:14:12 +0100

    Updating Maps Block

--- elementor-blocks/leap/maps-block.php
+++ elementor-blocks/leap/maps-block.php
@@ -51,13 +51,14 @@ class Widget_TommusRhodus_Maps_Block extends Widget_Base {
 				'options' => [
 					'basic'          	=> esc_html__( 'Basic', 'tr-framework' ),
 					'styled'         	=> esc_html__( 'Styled Map', 'tr-framework' ),
+					'styled_multi'		=> esc_html__( 'Styled Map + Multiple Markers', 'tr-framework' ),
 				],
 			]
 		);
 
 		$this->add_control(
 			'address', [
-				'label'       => __( 'Address', 'tr-framework' ),
+				'label'       => __( 'Address - Use this if your using the "basic" map.', 'tr-framework' ),
 				'type'        => Controls_Manager::TEXT,
 				'default'     => 'South Wharf, Melbourne Australia',
 				'label_block' => true
@@ -66,7 +67,7 @@ class Widget_TommusRhodus_Maps_Block extends Widget_Base {
 
 		$this->add_control(
 			'latlong', [
-				'label'       => __( 'Latitude & Logitude (eg 40.713750,-74.007590) - Use this if your using "styled" maps.', 'tr-framework' ),
+				'label'       => __( 'Latitude & Logitude (eg 40.713750,-74.007590) - Use this if your using "styled" map.', 'tr-framework' ),
 				'type'        => Controls_Manager::TEXT,
 				'default'     => '40.713750,-74.007590',
 				'label_block' => true
@@ -91,6 +92,25 @@ class Widget_TommusRhodus_Maps_Block extends Widget_Base {
 		);
 
 		$this->add_control(
+			'zoom', [
+				'label' => __( 'Default Zoom Level', 'tr-framework' ),
+				'type' => Controls_Manager::SLIDER,
+				'size_units' => [ 'px' ],
+				'range' => [
+					'px' => [
+						'min' => 1,
+						'max' => 20,
+						'step' => 1,
+					],
+				],
+				'default' => [
+					'unit' => 'px',
+					'size' => 14,
+				]
+			]
+		);
+
+		$this->add_control(
 			'show_zoom', [
 				'label' 		=> __( 'Show Zoom Controls', 'tr-framework' ),
 				'type' 			=> \Elementor\Controls_Manager::SWITCHER,
@@ -100,6 +120,53 @@ class Widget_TommusRhodus_Maps_Block extends Widget_Base {
 				'default' 		=> 'no',
 			]
 		);
+
+		$this->add_control(
+			'marker_image', [
+				'label' => __( 'Choose Image', 'tr-framework' ),
+				'type' => \Elementor\Controls_Manager::MEDIA,
+				'default' => [
+					'url' => get_template_directory_uri() .'/style/img/map-marker-2.svg',
+				],
+			]
+		);
+
+		$this->end_controls_section();
+
+		$this->start_controls_section(
+			'markers_section', [
+				'label' => __( 'Markers', 'tr-framework' )
+			]
+		);
+
+		$repeater = new Repeater();
+
+		$repeater->add_control(
+			'marker_latlong', [
+				'label'       => __( 'Latitude & Logitude (eg 40.713750,-74.007590)', 'tr-framework' ),
+				'type'        => Controls_Manager::TEXT,
+				'default'     => '40.713750,-74.007590',
+				'label_block' => true
+			]
+		);
+
+		$repeater->add_control(
+			'marker_content', [
+				'label'       => __( 'Marker Info', 'tr-framework' ),
+				'type'        => Controls_Manager::WYSIWYG,
+				'default'     => ''
+			]
+		);
+
+		$this->add_control(
+			'list', [
+				'label'   => __( 'Multiple Markers', 'tr-framework' ),
+				'type'    => Controls_Manager::REPEATER,
+				'fields'  => $repeater->get_controls(),
+				'default' => [],
+				'title_field' => __( 'Marker Details', 'tr-framework' ),
+			]
+		);		
 
 		$this->end_controls_section();
 
@@ -119,14 +186,12 @@ class Widget_TommusRhodus_Maps_Block extends Widget_Base {
 		if( 'basic' == $settings['layout'] ) {
 
 			echo '
-				<div class="min-vh-50 w-100 rounded" data-marker-image="'. get_template_directory_uri() .'/style/img/map-marker-2.svg" data-maps-api-key="'. $settings['api_key'] .'" data-address="'. $settings['address'] .'" data-map-zoom="14" '. $zoom .'>
+				<div class="min-vh-50 w-100 rounded" data-marker-image="'. $settings['marker_image']['url'] .'" data-maps-api-key="'. $settings['api_key'] .'" data-address="'. $settings['address'] .'" data-map-zoom="'. $settings['zoom']['size'] .'" '. $zoom .'>
 					<div class="map-marker" data-address="'. $settings['address'] .'">
 						<div class="info-window" data-max-width="400">
 							<div class="container">
 								<div class="row">
-									<div class="col-12">
 									'. $settings['content'] .'
-									</div>
 								</div>
 							</div>
 						</div>
@@ -137,14 +202,12 @@ class Widget_TommusRhodus_Maps_Block extends Widget_Base {
 		} elseif( 'styled' == $settings['layout'] ) {
 
 			echo '
-				<div class="min-vh-50 w-100 rounded" data-marker-image="'. get_template_directory_uri() .'/style/img/map-marker-2.svg" data-maps-api-key="'. $settings['api_key'] .'" data-latlong="'. $settings['latlong'] .'" data-map-zoom="14" '. $zoom .'>
+				<div class="min-vh-50 w-100 rounded" data-marker-image="'. $settings['marker_image']['url'] .'" data-maps-api-key="'. $settings['api_key'] .'" data-latlong="'. $settings['latlong'] .'" data-map-zoom="'. $settings['zoom']['size'] .'" '. $zoom .'>
 					<div class="map-marker" data-latlong="'. $settings['latlong'] .'">
 						<div class="info-window" data-max-width="400">
 							<div class="container">
 								<div class="row">
-									<div class="col-12">
-										'. $settings['content'] .'
-									</div>
+									'. $settings['content'] .'
 								</div>
 							</div>
 						</div>
@@ -156,8 +219,94 @@ class Widget_TommusRhodus_Maps_Block extends Widget_Base {
 				</div>
 	    	';
 			
+		} elseif( 'styled_multi' == $settings['layout'] ) {
+
+			echo '
+				<div class="min-vh-50 w-100 rounded" data-marker-image="'. $settings['marker_image']['url'] .'" data-maps-api-key="'. $settings['api_key'] .'" data-latlong="'. $settings['latlong'] .'" data-map-zoom="'. $settings['zoom']['size'] .'" '. $zoom .'>';
+
+					foreach( $settings['list'] as $item ){
+
+						echo '
+							<div class="map-marker" data-latlong="'. $item['marker_latlong'] .'">
+								<div class="info-window" data-max-width="400">
+									<div class="container">
+										<div class="row">
+											'. $item['marker_content'] .'
+										</div>
+									</div>
+								</div>
+							</div>
+						';
+
+					}
+
+					echo '
+	              	<div class="map-style">
+						'. $settings['style_code'] .'
+					</div>
+	            </div>
+	    	';
+			
 		}
 
+	}
+
+	protected function _content_template() {
+		?>
+
+			<# if ( 'basic' == settings.layout ) { #>
+
+				<div class="min-vh-50 w-100 rounded" data-marker-image="{{ settings.marker_image.url }}" data-maps-api-key="{{ settings.api_key }}" data-address="{{ settings.address }}" data-map-zoom="{{ settings.zoom.size }}">
+					<div class="map-marker" data-address="{{ settings.address }}">
+						<div class="info-window" data-max-width="400">
+							<div class="container">
+								<div class="row">
+									{{{ settings.content }}}
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+					
+			<# } else if ( 'styled' == settings.layout ) { #>
+
+				<div class="min-vh-50 w-100 rounded" data-marker-image="{{ settings.marker_image.url }}" data-maps-api-key="{{ settings.api_key }}" data-address="{{ settings.latlong }}" data-map-zoom="{{ settings.zoom.size }}">
+					<div class="map-marker" data-address="{{ settings.address }}">
+						<div class="info-window" data-max-width="400">
+							<div class="container">
+								<div class="row">
+									{{{ settings.content }}}
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="map-style">
+						{{{ settings.style_code }}}
+					</div>
+				</div>
+					
+			<# } else if ( 'styled_multi' == settings.layout ) { #>
+
+				<div class="min-vh-50 w-100 rounded" data-marker-image="{{ settings.marker_image.url }}" data-maps-api-key="{{ settings.api_key }}" data-address="{{ settings.latlong }}" data-map-zoom="{{ settings.zoom.size }}">
+					<# _.each( settings.list, function( item ) { #>
+						<div class="map-marker" data-address="{{ item.marker_latlong }}">
+							<div class="info-window" data-max-width="400">
+								<div class="container">
+									<div class="row">
+										{{{ item.marker_content }}}
+									</div>
+								</div>
+							</div>
+						</div>
+					<# }); #>
+					<div class="map-style">
+						{{{ settings.style_code }}}
+					</div>
+				</div>
+					
+			<# } #>
+		
+		<?php
 	}
 
 }
