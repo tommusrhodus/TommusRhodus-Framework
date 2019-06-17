c003272593371e4c453999eea6a9cd0e3d09a28d
TommusRhodus <hello@madeinebor.com>
2019-06-17 16:38:22 +0100

    Leap Tabbed Schedule Block

--- elementor-blocks/leap/tabbed-schedule-block.php
+++ elementor-blocks/leap/tabbed-schedule-block.php
@@ -46,6 +46,15 @@ class Widget_TommusRhodus_Tabbed_Schedule_Block extends Widget_Base {
 				'label' => __( 'Schedule Item', 'tr-framework' )
 			]
 		);
+		
+		$this->add_control(
+			'block_title', [
+				'label'       => __( 'Block Title', 'tr-framework' ),
+				'type'        => Controls_Manager::TEXT,
+				'default'     => 'Schedule',
+				'label_block' => true
+			]
+		);
 
 		$this->add_control(
 			'workshop_title', [
@@ -153,8 +162,53 @@ class Widget_TommusRhodus_Tabbed_Schedule_Block extends Widget_Base {
 		
 		$settings                = $this->get_settings_for_display();
 		$user_selected_animation = (bool) $settings['_animation'];
+		$days                    = array_column( $settings['list'], 'tab_title' );
+		$days_unique             = array_unique( $days );
+		
+		// If we don't have days, escape from this completely
+		if( !is_array( $days ) ){
+			return false;
+		}
 		
 		echo '
+			<div class="row aling-items-center mb-4">
+			
+				<div class="col">
+					<h2 class="h1 mb-sm-0">'. $settings['block_title'] .'</h2>
+				</div>
+		';
+		
+		if( is_array( $days_unique ) ){
+			
+			$i = 0;
+			
+			echo '<div class="col-auto"><ul class="nav" role="tablist">';
+			
+			foreach( $days_unique as $day ){
+				
+				$attr     = sanitize_title( $day );
+				$class    = ( 0 == $i ) ? 'active' : '';
+				$selected = ( 0 == $i ) ? 'true' : 'false';
+				
+				echo '
+					<li class="nav-item">
+						<a class="nav-link btn '. $class .'" href="#'. esc_attr( $attr ) .'" id="'. esc_attr( $attr ) .'-tab" data-toggle="tab" aria-controls="'. esc_attr( $attr ) .'" role="tab" aria-selected="'. $selected .'">
+							'. $day .'
+						</a>
+					</li>
+				';
+				
+				$i++;
+				
+			}
+			
+			echo '</ul></div>';
+				
+		}
+
+		echo '	
+			</div>
+			
 			<div class="row no-gutters d-none d-md-flex bg-white py-3">
 				<div class="col-xl-7 col-md-6">
 					<span class="h6 mb-0 text-muted">'. $settings['workshop_title'] .'</span>
@@ -166,10 +220,73 @@ class Widget_TommusRhodus_Tabbed_Schedule_Block extends Widget_Base {
 					<span class="h6 mb-0 text-muted">'. $settings['presenter_title'] .'</span>
 				</div>
 			</div>
+			
 			<div class="tab-content" data-aos="fade-up">
-
-			</div>
 		';
+		
+		$i = 0;
+		
+		foreach( $days_unique as $day ){
+			
+			$attr     = sanitize_title( $day );
+			$class    = ( 0 == $i ) ? 'show active' : '';
+			
+			echo '<div id="'. esc_attr( $attr ) .'" class="list-group list-group-flush tab-pane fade '. $class .'" role="tabpanel" aria-labelledby="'. esc_attr( $attr ) .'-tab">';
+			
+				foreach( $settings['list'] as $item ){
+					
+					if(!( $item['tab_title'] == $day )){
+						continue;
+					}
+					
+					$target   = $item['url']['is_external'] ? ' target="_blank"' : '';
+					$nofollow = $item['url']['nofollow']    ? ' rel="nofollow"'  : '';
+					$link     = 'href="'. esc_url( $item['url']['url'] ) .'"' . $target . $nofollow;
+					
+					echo '
+						<a '. $link .' class="list-group-item list-group-item-action row no-gutters align-items-center py-3">
+						
+							<div class="col-xl-7 col-md-6">
+								<h5 class="mb-0">'. $item['title'] .'</h5>
+							</div>
+							
+							<div class="col-md">
+								<span>'. $item['time'] .'</span>
+							</div>
+					';
+					
+					if( $item['author_image']['id'] ){
+						
+						echo '
+							<div class="col-md">
+								<div class="d-flex align-items-center mt-2 mt-md-0">
+									'. wp_get_attachment_image( $item['author_image']['id'], 'large', 0, array( 'class' => 'avatar avatar-sm mr-2' ) ) .'
+									<span class="h6 mb-0">'. $item['author_name'] .'</span>
+								</div>
+							</div>
+						';
+						
+					} else {
+						
+						echo '
+							<div class="col-md">
+							  <span>–</span>
+							</div>
+						';
+						
+					}
+							
+					echo '</a>';
+					
+				}
+				
+			echo '</div>';
+			
+			$i++;
+				
+		}
+				
+		echo '</div>';
 
 	}
 
