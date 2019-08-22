fcd691a148a29a95dc7da6ed4c43e8d3f227a34b
Daniel Jones <distinctivedan@outlook.com>
2019-08-22 12:01:27 +0100

    Fixing Accordions IDs

--- elementor-blocks/uptime/accordion-block.php
+++ elementor-blocks/uptime/accordion-block.php
@@ -86,14 +86,14 @@ class Widget_TommusRhodus_Accordion_Block extends Widget_Base {
 		
 			echo '
 				<div class="border-bottom pb-3 mb-3">
-					<div data-target="#'. esc_attr( $attr_title ) .'" class="accordion-panel-title" data-toggle="collapse" role="button" aria-expanded="false" aria-controls="'. esc_attr( $attr_title ) .'">
+					<div data-target="#'. sanitize_file_name( $attr_title ) .'" class="accordion-panel-title" data-toggle="collapse" role="button" aria-expanded="false" aria-controls="'. sanitize_file_name( $attr_title ) .'">
 						<span class="h6 mb-0">'. $title .'</span>
 						<svg class="icon" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 							<path d="M19 11H5C4.44772 11 4 11.4477 4 12C4 12.5523 4.44772 13 5 13H19C19.5523 13 20 12.5523 20 12C20 11.4477 19.5523 11 19 11Z" fill="#212529" />
 							<path d="M13 19L13 5C13 4.44772 12.5523 4 12 4C11.4477 4 11 4.44772 11 5L11 19C11 19.5523 11.4477 20 12 20C12.5523 20 13 19.5523 13 19Z" fill="#212529" />
 						</svg>
 					</div>
-					<div class="collapse" id="'. esc_attr( $attr_title ) .'">
+					<div class="collapse" id="'. sanitize_file_name( $attr_title ) .'">
 						<div class="pt-3">
 							<p class="mb-0">
 								'. $settings['item_content'] .'
@@ -107,14 +107,14 @@ class Widget_TommusRhodus_Accordion_Block extends Widget_Base {
 		
 			echo '
 				<div class="border-bottom pb-3 mb-3">
-					<div data-target="#'. esc_attr( $attr_title ) .'" class="accordion-panel-title" data-toggle="collapse" role="button" aria-expanded="true" aria-controls="'. esc_attr( $attr_title ) .'">
+					<div data-target="#'. sanitize_file_name( $attr_title ) .'" class="accordion-panel-title" data-toggle="collapse" role="button" aria-expanded="true" aria-controls="'. sanitize_file_name( $attr_title ) .'">
 						<span class="h6 mb-0">'. $title .'</span>
 						<svg class="icon" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 							<path d="M19 11H5C4.44772 11 4 11.4477 4 12C4 12.5523 4.44772 13 5 13H19C19.5523 13 20 12.5523 20 12C20 11.4477 19.5523 11 19 11Z" fill="#212529" />
 							<path d="M13 19L13 5C13 4.44772 12.5523 4 12 4C11.4477 4 11 4.44772 11 5L11 19C11 19.5523 11.4477 20 12 20C12.5523 20 13 19.5523 13 19Z" fill="#212529" />
 						</svg>
 					</div>
-					<div class="collapse show" id="'. esc_attr( $attr_title ) .'">
+					<div class="collapse show" id="'. sanitize_file_name( $attr_title ) .'">
 						<div class="pt-3">
 							<p class="mb-0">
 								'. $settings['item_content'] .'
@@ -129,7 +129,7 @@ class Widget_TommusRhodus_Accordion_Block extends Widget_Base {
 			echo '
 				<div class="card mb-2 card-sm card-body hover-shadow-sm">
 				
-					<div data-target="#'. esc_attr( $attr_title ) .'" class="accordion-panel-title" data-toggle="collapse" role="button" aria-expanded="false" aria-controls="'. esc_attr( $attr_title ) .'">
+					<div data-target="#'. sanitize_file_name( $attr_title ) .'" class="accordion-panel-title" data-toggle="collapse" role="button" aria-expanded="false" aria-controls="'. sanitize_file_name( $attr_title ) .'">
 						<span class="h6 mb-0">'. $title .'</span>
 						<svg class="icon" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 							<path d="M19 11H5C4.44772 11 4 11.4477 4 12C4 12.5523 4.44772 13 5 13H19C19.5523 13 20 12.5523 20 12C20 11.4477 19.5523 11 19 11Z" fill="#212529" />
@@ -137,7 +137,7 @@ class Widget_TommusRhodus_Accordion_Block extends Widget_Base {
 						</svg>
 					</div>
 					
-					<div class="collapse" id="'. esc_attr( $attr_title ) .'">
+					<div class="collapse" id="'. sanitize_file_name( $attr_title ) .'">
 						<div class="pt-3">
 							<p class="mb-0">'. $settings['item_content'] .'</p>
 						</div>
