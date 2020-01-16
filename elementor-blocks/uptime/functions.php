a56ffac32e68bc13443628e43dccec4de587b306
TommusRhodus <hello@madeinebor.com>
2020-01-16 10:39:20 +0000

    Uptime Elementor Pro support

--- elementor-blocks/uptime/functions.php
+++ elementor-blocks/uptime/functions.php
@@ -1,5 +1,13 @@
 <?php 
 
+function tommusrhodus_framework_register_elementor_locations( $elementor_theme_manager ) {
+
+	$elementor_theme_manager->register_location( 'header' );
+	$elementor_theme_manager->register_location( 'footer' );
+
+}
+add_action( 'elementor/theme/register_locations', 'tommusrhodus_framework_register_elementor_locations', 10, 1 );
+
 function tommusrhodus_framework_add_elementor_widget_categories( $elements_manager ) {
 	
 	$elements_manager->add_category(
