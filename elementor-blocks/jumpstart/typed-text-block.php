62385b911d40472fe76686dad9d050ac8262f4af
TommusRhodus <hello@madeinebor.com>
2020-02-20 09:56:13 +0000

    Update typed-text-block.php

--- elementor-blocks/jumpstart/typed-text-block.php
+++ elementor-blocks/jumpstart/typed-text-block.php
@@ -114,7 +114,7 @@ class Widget_TommusRhodus_Typed_Text_Block extends Widget_Base {
 		);	
 		
 		$this->add_control(
-			'prefix', [
+			'text_speed', [
 				'label'       => __( 'Text Speed', 'tr-framework' ),
 				'type'        => Controls_Manager::TEXT,
 				'default'     => '45',
