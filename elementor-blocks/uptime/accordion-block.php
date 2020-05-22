58b89f0db4f973ea2b717812a1216fa4474ec0b0
TommusRhodus <hello@madeinebor.com>
2020-05-22 17:22:16 +0100

    Fixes Uptime #89

--- elementor-blocks/uptime/accordion-block.php
+++ elementor-blocks/uptime/accordion-block.php
@@ -74,7 +74,8 @@ class Widget_TommusRhodus_Accordion_Block extends Widget_Base {
 	
 		$settings   = $this->get_settings_for_display();
 		$title      = $settings['item_title'];
-		$attr_title = sanitize_title_with_dashes( $title );
+		$attr_title = sanitize_file_name( sanitize_title_with_dashes( $title ) );
+		$attr_id    = trim( preg_replace( '/\b\d+\b/', '', $attr_title ) );
 		
 		$user_selected_animation = (bool) $settings['_animation'];
 		
@@ -86,14 +87,14 @@ class Widget_TommusRhodus_Accordion_Block extends Widget_Base {
 		
 			echo '
 				<div class="border-bottom pb-3 mb-3">
-					<div data-target="#'. sanitize_file_name( $attr_title ) .'" class="accordion-panel-title" data-toggle="collapse" role="button" aria-expanded="false" aria-controls="'. sanitize_file_name( $attr_title ) .'">
+					<div data-target="#'. $attr_id .'" class="accordion-panel-title" data-toggle="collapse" role="button" aria-expanded="false" aria-controls="'. $attr_id .'">
 						<span class="h6 mb-0">'. $title .'</span>
 						<svg class="icon" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 							<path d="M19 11H5C4.44772 11 4 11.4477 4 12C4 12.5523 4.44772 13 5 13H19C19.5523 13 20 12.5523 20 12C20 11.4477 19.5523 11 19 11Z" fill="#212529" />
 							<path d="M13 19L13 5C13 4.44772 12.5523 4 12 4C11.4477 4 11 4.44772 11 5L11 19C11 19.5523 11.4477 20 12 20C12.5523 20 13 19.5523 13 19Z" fill="#212529" />
 						</svg>
 					</div>
-					<div class="collapse" id="'. sanitize_file_name( $attr_title ) .'">
+					<div class="collapse" id="'. $attr_id .'">
 						<div class="pt-3">
 							<p class="mb-0">
 								'. $settings['item_content'] .'
@@ -107,14 +108,14 @@ class Widget_TommusRhodus_Accordion_Block extends Widget_Base {
 		
 			echo '
 				<div class="border-bottom pb-3 mb-3">
-					<div data-target="#'. sanitize_file_name( $attr_title ) .'" class="accordion-panel-title" data-toggle="collapse" role="button" aria-expanded="true" aria-controls="'. sanitize_file_name( $attr_title ) .'">
+					<div data-target="#'. $attr_id .'" class="accordion-panel-title" data-toggle="collapse" role="button" aria-expanded="true" aria-controls="'. $attr_id .'">
 						<span class="h6 mb-0">'. $title .'</span>
 						<svg class="icon" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 							<path d="M19 11H5C4.44772 11 4 11.4477 4 12C4 12.5523 4.44772 13 5 13H19C19.5523 13 20 12.5523 20 12C20 11.4477 19.5523 11 19 11Z" fill="#212529" />
 							<path d="M13 19L13 5C13 4.44772 12.5523 4 12 4C11.4477 4 11 4.44772 11 5L11 19C11 19.5523 11.4477 20 12 20C12.5523 20 13 19.5523 13 19Z" fill="#212529" />
 						</svg>
 					</div>
-					<div class="collapse show" id="'. sanitize_file_name( $attr_title ) .'">
+					<div class="collapse show" id="'. $attr_id .'">
 						<div class="pt-3">
 							<p class="mb-0">
 								'. $settings['item_content'] .'
@@ -129,7 +130,7 @@ class Widget_TommusRhodus_Accordion_Block extends Widget_Base {
 			echo '
 				<div class="card mb-2 card-sm card-body hover-shadow-sm">
 				
-					<div data-target="#'. sanitize_file_name( $attr_title ) .'" class="accordion-panel-title" data-toggle="collapse" role="button" aria-expanded="false" aria-controls="'. sanitize_file_name( $attr_title ) .'">
+					<div data-target="#'. $attr_id .'" class="accordion-panel-title" data-toggle="collapse" role="button" aria-expanded="false" aria-controls="'. $attr_id .'">
 						<span class="h6 mb-0">'. $title .'</span>
 						<svg class="icon" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 							<path d="M19 11H5C4.44772 11 4 11.4477 4 12C4 12.5523 4.44772 13 5 13H19C19.5523 13 20 12.5523 20 12C20 11.4477 19.5523 11 19 11Z" fill="#212529" />
@@ -137,7 +138,7 @@ class Widget_TommusRhodus_Accordion_Block extends Widget_Base {
 						</svg>
 					</div>
 					
-					<div class="collapse" id="'. sanitize_file_name( $attr_title ) .'">
+					<div class="collapse" id="'. $attr_id .'">
 						<div class="pt-3">
 							<p class="mb-0">'. $settings['item_content'] .'</p>
 						</div>
